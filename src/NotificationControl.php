9e8767a46bd391d97e2619dfec689203bdf97dac
Simon <2222031+sdiazsaez@users.noreply.github.com>
2025-05-19 02:08:45 +0000

    first

---
+++ src/NotificationControl.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Cosmoscript\NotificationControl;
+
+class NotificationControl
+{
+    protected static bool $disabled = false;
+
+    public static function disable(): void
+    {
+        self::$disabled = true;
+    }
+
+    public static function enable(): void
+    {
+        self::$disabled = false;
+    }
+
+    public static function isDisabled(): bool
+    {
+        return self::$disabled;
+    }
+
+    /**
+     * Run a callback with notifications suppressed
+     */
+    public static function whileDisabled(callable $callback)
+    {
+        self::disable();
+
+        try {
+            return $callback();
+        } finally {
+            self::enable();
+        }
+    }
+}
